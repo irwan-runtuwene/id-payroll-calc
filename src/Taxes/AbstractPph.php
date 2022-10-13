c61ec1763f84e26e3338d6e37ab9f9bed86aef9b
Loetfi <lutfi@mhs.ubharajaya.ac.id>
2022-10-13 11:08:34 +0700

    Update AbstractPph.php

--- src/Taxes/AbstractPph.php
+++ src/Taxes/AbstractPph.php
@@ -50,7 +50,7 @@ abstract class AbstractPph
         $this->calculator =& $calculator;
         $this->result = new SplArrayObject([
             'ptkp' => new SplArrayObject([
-                'status' => $this->calculator->provisions->state->getPtkp($this->calculator->employee->numOfDependentsFamily, $this->calculator->employee->maritalStatus),
+                'status' => $this->calculator->employee->ptkpType ?? 'TK/0',
                 'amount' => 0
             ]),
             'pkp' => 0,
